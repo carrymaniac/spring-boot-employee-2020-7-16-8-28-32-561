1615975d2d6b20d3798a3dd605e303f3c3697d9b
OOCLDM\HUANGHE7 <henry.huang@oocl.com>
2020-07-28 20:34:33 +0800

    [Business Requirement] [henry] factory: modify updateCompany method

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -45,8 +45,9 @@ public class CompanyController {
         return "success";
     }
 
-    @PutMapping
-    String updateCompany(@RequestBody Company newCompany) {
+    @PutMapping("{id}")
+    String updateCompany(@RequestBody Company newCompany,@PathVariable("id") String id) {
+        newCompany.setId(id);
         Company oldCompany = this.companyService.getCompanys().stream()
                 .filter(company -> company.getId().equals(newCompany.getId()))
                 .findFirst().orElse(null);
